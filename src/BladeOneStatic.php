7c8494f5f6367be5e70c1676c054ddebffc1d4a6
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-04-08 14:32:42 +0200

    init

---
+++ src/BladeOneStatic.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace LaravelBladeOneStatic;
+
+Use eftec\bladeone\BladeOne as Blade;
+
+/**
+ * Class BladeOneStatic
+ * @package LaravelBladeOneStatic
+ */
+class BladeOneStatic
+{
+    /**
+     * @var
+     */
+    static $bladeOne;
+
+    /**
+     * @return Blade
+     */
+    public static function init()
+    {
+        self::$bladeOne = new Blade(
+            (array)\BladeComponentLibrary\Register::$viewPaths,
+            (string)\BladeComponentLibrary\Register::$cachePath
+        );
+
+        return self::$bladeOne;
+    }
+
+
+    /** Init Blade Engine
+     * @return string
+     * @throws \Exception
+     */
+    public static function initBladeOne($params)
+    {
+        if (!class_exists(Blade::class)) {
+            return false;
+        }
+
+        if (class_exist(\BladeComponentLibrary\Register)) {
+
+            return self::$bladeOne->run(
+                (string)$params['utilityViewName'],
+                (array)$params['utilityArgsControlerData']
+            );
+
+        } else {
+            throw new \Exception("Error running Blade one");
+        }
+    }
+
+
+}
+
+
